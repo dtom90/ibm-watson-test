70d28bf819d6351b59771a249730a077f5d25e0b
David Thomason <dtom90@users.noreply.github.com>
2020-10-05 09:56:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const AssistantV1 = require('ibm-watson/assistant/v1')
+
+const assistant = new AssistantV1({
+  serviceName: 'assistant', // THIS IS REQUIRED TO WORK
+  version: '2020-04-01'
+});
+
+assistant.message({
+  workspaceId: '<workspaceId>',
+  input: {'text': 'Hello'},
+  headers: {
+    'Custom-Header': 'custom',
+    'Accept-Language': 'custom'
+  }
+})
+  .then(response => {
+    console.log(JSON.stringify(response.result, null, 2));
+  })
+  .catch(err => {
+    console.log('error: ', err);
+  });
